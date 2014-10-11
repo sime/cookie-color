6323aa8a03f58b3db308a884f0800d2179bf9db3
Simon Males <sime@sime.net.au>
2014-10-11 21:55:27 +0200

    Colour validating Angular app

---
+++ js/app.js
@@ -0,0 +1,21 @@
+(function() {
+  var app = angular.module('cookies', []);
+
+  app.directive('color', function() {
+    return {
+      require: 'ngModel',
+      link: function(scope, elm, attrs, ctrl) {
+        ctrl.$parsers.unshift(function(viewValue) {
+          if (/(green|blue|red|yellow)/i.test(viewValue)) {
+            ctrl.$setValidity('color', true);
+            return viewValue;
+          } else {
+            ctrl.$setValidity('color', false);
+            return undefined;
+          }
+        })
+      }
+    };
+  });
+})();
+
